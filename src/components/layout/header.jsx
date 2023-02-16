abe2c9d04bf5de604859a8e845a91b8e37bee8e1
abhishek <abhishek.bhattarai61067@gmail.com>
2023-02-16 22:07:27 +0545

    feature: dark theme completed

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,6 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/theme';
+
 
 const Header = () => {
+  const [{isDark, theme}, toggleTheme] = useContext(ThemeContext);
   return (
     <header id="page-topbar">
       <div className="navbar-header">
@@ -18,7 +22,15 @@ const Header = () => {
             </button>
           </div>
           <div className="header-navigation">
-<FontAwesomeIcon icon="fa-solid fa-sun" />
+<button className='theme-icon mx-4' onClick={toggleTheme}>
+{
+  isDark?
+              (<FontAwesomeIcon icon="fa-solid fa-sun" className="p-1"/>)
+:
+           ( <FontAwesomeIcon icon="fa-solid fa-moon" className="p-1"/>)
+
+}
+</button>
             <FontAwesomeIcon icon="fa-solid fa-user" className="p-1"/>
             <FontAwesomeIcon icon="fa-solid fa-gear" className="p-1"/>
           </div>
